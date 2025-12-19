b4024a34fd40ee20372a679f2db6c65235bb3d9a
MD-Sahil-Alam <mdsahil.prof@gmail.com>
2025-12-20 00:32:45 +0530

    upadating abtraction

--- Abstraction/Main.java
+++ Abstraction/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Abstraction;
 abstract class Main{
   public String name = "sahil";
@@ -12,19 +11,3 @@ class Students extends Main{
     System.out.println("hii");
   } 
 }
-=======
-package Abstraction;
-abstract class Main {
-  public String fname = "John";
-  public int age = 24;
-  public abstract void study(); // abstract method
-}
-
-// Subclass (inherit from Main)
-class Student extends Main {
-  public int graduationYear = 2018;
-  public void study() { // the body of the abstract method is provided here
-    System.out.println("Studying all day long");
-  }
-}
->>>>>>> 53ce0c80778d9eb28caacbbd1bdae65b75e32cc2
--- Abstraction/second.java
+++ Abstraction/second.java
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 package Abstraction;
-
 public class Second{
   public static void main(String[] args) {
       Students s1 = new Students();
@@ -9,17 +7,3 @@ public class Second{
       System.out.println(s1.gyear);
       s1.sayhi();
   }
-=======
-package Abstraction;
-class Second {
-  public static void main(String[] args) {
-    // create an object of the Student class (which inherits attributes and methods from Main)
-    Student myObj = new Student();
-
-    System.out.println("Name: " + myObj.fname);
-    System.out.println("Age: " + myObj.age);
-    System.out.println("Graduation Year: " + myObj.graduationYear);
-    myObj.study(); // call abstract method
-  }
->>>>>>> 53ce0c80778d9eb28caacbbd1bdae65b75e32cc2
-}
