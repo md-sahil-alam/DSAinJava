f327e44130d8a54cf01699127fe0f69b8910261b
MD-Sahil-Alam <mdsahil.prof@gmail.com>
2025-12-20 17:03:41 +0530

    adding access modifier Protcted

--- Inheritence.java
+++ Inheritence.java
@@ -1,15 +1,16 @@
 class Parents{
-  String lastName="alam";
-  public void Hair(){
+  protected String lastName ="alam";//child can access protected variable of parents
+  protected String property = "7cr"; 
+  protected void Hair(){
     System.out.println("Blond");
   }
 }
 
-
 class Child extends Parents{
   public static void main(String[]args){
     Child child1= new Child();
-    System.out.println(child1.lastName);
+    System.out.println(child1.lastName);//child can access protected variable of parents
+    System.out.println(child1.property);
     child1.Hair();
   }
 }
