93d15ed1c762e7ee384111094656373ce26daacf
md-sahil-alam <mdsahil.prof@gmail.com>
2025-12-25 23:57:07 +0530

    add findInMountainArray hard  leet code

--- BinarySearch/findInMountainArray.java
+++ BinarySearch/findInMountainArray.java
@@ -7,21 +7,43 @@ public class findInMountainArray {
     int target = 3;
     System.out.println(find(arr,target)); 
   }
+
   static int find(int [] arr, int target){
+    int ans = -1;
     int start = 0;
     int end = arr.length-1;
     while (start<end) { 
         int mid = start+(end-start)/2;
         if (arr[mid]==target) {
-            return mid;
-        }else if (arr[mid]>target) {
-            end = mid-1;
+          if (mid<ans) {
+            ans =  mid;
+            
+          } 
         }else{
-          start = mid+1;
+            ans = asearch(arr,target,mid);
+            System.out.println(ans);
+            break;
         }
-    }
-    return -1;
+      }
+    return ans;
   }
 
-  
+  static int asearch(int[] arr , int target, int end){
+    int ans = -1;
+    int start = 0;
+    while (start<end) { 
+        int mid = start+(end-start)/2;
+        if (arr[mid]==target) {
+          if (mid<ans) {
+            System.out.println(mid);
+            return mid;
+          } 
+    }else if (arr[mid]>target) {
+      end = mid-1;
+    }else{
+      start = mid + 1;
+    }
+  }
+  return -1;
+}
 }
