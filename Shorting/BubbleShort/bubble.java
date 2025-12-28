e4f6226d3cdcae07eb2fdd9df5d2b8330f09184f
md-sahil-alam <mdsahil.prof@gmail.com>
2025-12-28 23:56:21 +0530

    update

--- Shorting/BubbleShort/bubble.java
+++ Shorting/BubbleShort/bubble.java
@@ -3,19 +3,25 @@ package Shorting.BubbleShort;
 import java.util.Arrays;
 
 public class bubble {
-  public static void main(String[] args) {
-    int[] arr = {3,1,5,4,2};
-    int end = arr.length-1;
-    while(end>0){
-      for (int i = 0; i < arr.length-1; i++) {
-        if (arr[i]>arr[i+1]) {
-          int temp = arr[i];
-          arr[i]=arr[i+1];
-          arr[i+1]= temp;
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 5, 4, 2};
+        int end = arr.length - 1;
+        boolean swapped;
+        while (end > 0) {
+            swapped = false;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+            end--;
         }
-      }
-      end--;
+        System.out.println(Arrays.toString(arr));
     }
-    System.out.println(Arrays.toString(arr));
-  }
 }
